dabd784269f09d8d49ccca162253a0babcd55aad
Yogitha <yogithas2807@gmail.com>
2026-07-31 09:47:26 +0530

    add three lines

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,8 @@ public class WelcomeToJava {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Java Track");
+        System.out.println("I already know how to build the logic");
+        System.out.println("I am here to learn how Java works");
+        System.out.println("Looking forward to becoming a Java Developer");
     }
 }
